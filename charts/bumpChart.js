4707f040e85bfb2ae5dc7e8db608efad39a2455f
Doug Brown <douglas.brown@mattersight.com>
2016-11-03 17:25:56 -0500

    cleany

--- charts/bumpChart.js
+++ charts/bumpChart.js
@@ -16,10 +16,11 @@
 		.required(1)
 	; 
 
-    var size = stream.dimension()
-        .title('Size')
-        .types(Number)
-        .required(1);
+	var size = stream.dimension()
+		.title('Size')
+		.types(Number)
+		.required(1)
+	;
 
 	stream.map(function (data){
 		if (!group()) return [];
@@ -30,69 +31,79 @@
 			//.key(group)
 			.key(function(d) {return group(d);})
 			.rollup(function (g) {
-                var singles = d3.nest()
-                    .key(function(d) {
-                    	var zzz1 = date(d);
-                    	return date(d);
-                    })
-                    .rollup(function (d){
-                    	var zzz2 = size(d[0]);
-                    	var zzz3 = d3.sum(d,size);
-                        return {
-                            group : group(d[0]),//d[0] because d is an array of one object.
-                            x : date(d[0]),
-                            y : size() ? d3.sum(d,size) : d.length
-                            //y : size(d[0])
-                        };
-                    })
-                    .map(g);
+				var singles = d3.nest()
+					.key(function(d) {
+						var zzz1 = date(d);
+						return date(d);
+					})
+					.rollup(function (d){
+						var zzz2 = size(d[0]);
+						var zzz3 = d3.sum(d,size);
+						return {
+							group : group(d[0]),//d[0] because d is an array of one object.
+							x : date(d[0]),
+							y : size() ? d3.sum(d,size) : d.length
+							//y : size(d[0])
+						};
+					})
+					.map(g)
+				;
 
-                // let's create the empty ones
-                dates.forEach(function(d){
-                    if (!singles.hasOwnProperty(d)) {
-                        singles[d] = { group : group(g[0]), x : d, y : 0 };
-                    }
-                });
+				// let's create the empty ones
+				dates.forEach(function(d){
+					if (!singles.hasOwnProperty(d)) {
+						singles[d] = {
+							group : group(g[0]),
+							x : d,
+							y : 0
+						};
+					}
+				});
 
-                return d3.values(singles);
-            })
-            .map(data);
+				return d3.values(singles);
+			})
+			.map(data)
+		;
 
-			var xxx = d3.values(groups)//gets an array of the points for each group
-				.map(function(d) {
-					return d.sort(function(a,b) {
-						return a.x - b.x; //this orders the points for each group by date.
-					});
-				})
-			;
-        return d3.values(groups).map(function(d){ return d.sort(function(a,b){ return a.x - b.x; }); });
+		var xxx = d3.values(groups)//gets an array of the points for each group
+			.map(function(d) {
+				return d.sort(function(a,b) {
+					return a.x - b.x; //this orders the points for each group by date.
+				});
+			})
+		;
+		
+		return d3.values(groups).map(function(d){ return d.sort(function(a,b){ return a.x - b.x; }); });
+	});
 
-    });
+	var chart = raw.chart()
+		.title('Bump Chart')
+		.thumbnail("imgs/bumpChart.png")
+		.description("For continuous data such as time series, a bump chart can be used in place of stacked bars. Based on New York Times's <a href='http://www.nytimes.com/interactive/2014/08/13/upshot/where-people-in-each-state-were-born.html'>interactive visualization.</a>")
+		.category('Time series')
+		.model(stream)
+	;
 
-    var chart = raw.chart()
-        .title('Bump Chart')
-        .thumbnail("imgs/bumpChart.png")
-        .description(
-            "For continuous data such as time series, a bump chart can be used in place of stacked bars. Based on New York Times's <a href='http://www.nytimes.com/interactive/2014/08/13/upshot/where-people-in-each-state-were-born.html'>interactive visualization.</a>")
-        .category('Time series')
-        .model(stream);
-
-    var width = chart.number()
-        .title("Width")
-        .defaultValue(1400)
-        .fitToWidth(false);
-
-    var height = chart.number()
-        .title("Height")
-        .defaultValue(800);
-
-    var padding = chart.number()
-        .title("Padding")
-        .defaultValue(1);
-
-    var normalize = chart.checkbox()
-        .title("Normalize")
-        .defaultValue(false);
+	var width = chart.number()
+		.title("Width")
+		.defaultValue(1400)
+		.fitToWidth(false)
+	;
+	
+	var height = chart.number()
+		.title("Height")
+		.defaultValue(800)
+	;
+	
+	var padding = chart.number()
+		.title("Padding")
+		.defaultValue(1)
+	;
+	
+	var normalize = chart.checkbox()
+		.title("Normalize")
+		.defaultValue(false)
+	;
 
 	var curve = chart.list()
 		.title("Interpolation")
@@ -100,23 +111,27 @@
 		.defaultValue('Basis')
 	;
 
-    var sort = chart.list()
-        .title("Sort by")
-        .values(['value (descending)', 'value (ascending)', 'group'])
-        .defaultValue('value (descending)');
+	var sort = chart.list()
+		.title("Sort by")
+		.values(['value (descending)', 'value (ascending)', 'group'])
+		.defaultValue('value (descending)')
+	;
+	
+	var showLabels = chart.checkbox()
+		.title("Show labels")
+		.defaultValue(true)
+	;
+	
+	var showGrid = chart.checkbox()
+		.title("Show grid")
+		.defaultValue(true)
+	;
+	
+	var colors = chart.color()
+		.title("Color scale")
+	;
 
-    var showLabels = chart.checkbox()
-        .title("Show labels")
-        .defaultValue(true);
-
-    var showGrid = chart.checkbox()
-        .title("Show grid")
-        .defaultValue(true);
-
-    var colors = chart.color()
-        .title("Color scale");
-
-    chart.draw(function (selection, data){
+	chart.draw(function (selection, data){
 
 		var g = selection
 			.attr("width", +width() )
@@ -137,13 +152,13 @@
 			*/
 			.append("g")
 		;
-        var layers = data;
+		var layers = data;
 
-        var curves = {
-          'Basis' : 'basis',
-          'Sankey' : interpolate,
-          'Linear' : 'linear'
-        };
+		var curves = {
+			'Basis' : 'basis',
+			'Sankey' : interpolate,
+			'Linear' : 'linear'
+		};
 
 		layers[0].forEach(function(d,i){
 			var values = layers.map(function(layer){
@@ -232,23 +247,25 @@
 			});
 		});
 
-        var xAxis = d3.svg.axis().scale(x).tickSize(-height()+20).orient("bottom");
+		var xAxis = d3.svg.axis().scale(x).tickSize(-height()+20).orient("bottom");
 
-        g.append("g")
-            .attr("class", "x axis")
-            .style("stroke-width", "1px")
-            .style("font-size","10px")
-            .style("font-family","Arial, Helvetica")
-            .attr("transform", "translate(" + 0 + "," + (height()-20) + ")")
-            .style("display",function(){ return showGrid() ? 'block' : 'none'; })
-            .call(xAxis);
+		g.append("g")
+			.attr("class", "x axis")
+			.style("stroke-width", "1px")
+			.style("font-size","10px")
+			.style("font-family","Arial, Helvetica")
+			.attr("transform", "translate(" + 0 + "," + (height()-20) + ")")
+			.style("display",function(){ return showGrid() ? 'block' : 'none'; })
+			.call(xAxis)
+		;
 
-        d3.selectAll(".x.axis line, .x.axis path")
-            .style("shape-rendering","crispEdges")
-            .style("fill","none")
-            .style("stroke","#ccc");
-
-        colors.domain(layers, function (d){ return d[0].group; });
+		d3.selectAll(".x.axis line, .x.axis path")
+			.style("shape-rendering","crispEdges")
+			.style("fill","none")
+			.style("stroke","#ccc")
+		;
+		
+		colors.domain(layers, function (d) {return d[0].group;});
 
 		var area = d3.svg.area()
 			.interpolate(curves[curve()])
@@ -256,7 +273,8 @@
 			.y0(function(d) { return y(d.y0); })
 			.y1(function(d) {
 				return Math.min(y(d.y0)-1, y(d.y0 + d.y));//The range is upside down
-			});
+			})
+		;
 
 		var line = d3.svg.line()
 			.interpolate(curves[curve()])
@@ -284,6 +302,7 @@
 				})
 			;
 		}
+		
 		function unHighlight() {
 			g.selectAll("path.layer")
 				.style("fill-opacity", .7)
@@ -296,7 +315,6 @@
 			;
 		}
 
-		//var mouseoverTimeout = null;
 		g.selectAll("path.layer")
 			.data(layers)
 			.enter().append("path")
@@ -329,88 +347,84 @@
 						}
 					}
 				})
-			;
-
-        if (!showLabels()) return;
-
-        g.append('defs');
-
-		function textpathName(d, i) {
-			return 'path-' + i;
-		}
-		function textpathHref(d, i) {
-			return '#path-' + i;
-		}
-		
-		g.select('defs').selectAll('path').data(layers).enter().append('path')
-			//.attr('id', function(d,i) { return 'path-' + i; })
-			.attr('id', textpathName)
-			.attr('d', line)
 		;
 
-        g.selectAll("text.label").data(layers).enter().append('text')
-            .attr('dy', '0.5ex')
-            .attr("class","label")
-            .append('textPath')
-            	.attr("id", "topicLabel")
-            	.attr('xlink:xlink:href', textpathHref)
-            	.attr('startOffset', function(d, i) {
-                	var maxYr = 0, maxV = 0;
-                	d3.range(layers[0].length).forEach(function(i) {//Find the fatest part of the shape
-                    	if (d[i].y > maxV) {
-                        	maxV = d[i].y;
-                        	maxYr = i;
-                    	}
-                	});
-                	//d.maxVal = d[maxYr].y;Don't need this
-                	d.offset = Math.round(x(d[maxYr].x) / x.range()[1] * 100);
-                	return Math.min(95, Math.max(5, d.offset))+'%';
-                	d.offset = x(d[maxYr].x);
-                	
-                	var id = textpathHref(d, i);
-                	var thePath = g.select('defs').select(id).node();
-                	console.log("thePath");
-                	console.log(id);
-                	console.log(thePath);
-                	console.log("x="+x(d[maxYr].x));
-                	console.log("offset="+d.offset);
-                	console.log(thePath.getPointAtLength(d.offset).x);
-                	while (thePath.getPointAtLength(d.offset).x < x(d[maxYr].x)) {
-                		d.offset += 1;
-//                		console.log("offset="+d.offset);
-//                		console.log(thePath.getPointAtLength(d.offset));
-                	}
-                	return d.offset;
-            	})
-            	.attr('text-anchor', function(d) {
-//                	return d.offset <= x.range()[0] ? 'start' : d.offset >= x.range()[1] ? 'end' : 'middle';
-//                	return 'middle';
-                	return d.offset > 90 ? 'end' : d.offset < 10 ? 'start' : 'middle';
-            	})
-            	.text(function(d){ return d[0].group; })
-            	.style("font-size","11px")
-            	.style("font-family","Arial, Helvetica")
-            	.style("font-weight","normal");
+		if (showLabels()) {
 
-        function sortBy(a,b){
-            if (sort() == 'value (descending)') return a.y - b.y;
-            if (sort() == 'value (ascending)') return b.y - a.y;
-            if (sort() == 'group') return a.group - b.group;
-        }
+			g.append('defs');
 
-        function interpolate(points) {
-          var x0 = points[0][0], y0 = points[0][1], x1, y1, x2,
-              path = [x0, ",", y0],
-              i = 0,
-              n = points.length;
+			function textpathName(d, i) {return 'path-' + i;}
+			function textpathHref(d, i) {return '#' + textpathName(d,i);}
+		
+			g.select('defs').selectAll('path').data(layers).enter().append('path')
+				//.attr('id', function(d,i) { return 'path-' + i; })
+				.attr('id', textpathName)
+				.attr('d', line)
+			;
 
-          while (++i < n) {
-            x1 = points[i][0], y1 = points[i][1], x2 = (x0 + x1) / 2;
-            path.push("C", x2, ",", y0, " ", x2, ",", y1, " ", x1, ",", y1);
-            x0 = x1, y0 = y1;
-          }
-          return path.join("");
-        }
-    });
+			g.selectAll("text.label").data(layers).enter().append('text')
+				.attr('dy', '0.5ex')
+				.attr("class","label")
+				.append('textPath')
+					.attr("id", "topicLabel")
+					.attr('xlink:xlink:href', textpathHref)
+					.attr('startOffset', function(d, i) {
+						var maxYr = 0, maxV = 0;
+						d3.range(layers[0].length).forEach(function(i) {//Find the fatest part of the shape
+							if (d[i].y > maxV) {
+								maxV = d[i].y;
+								maxYr = i;
+							}
+						});
+						//d.maxVal = d[maxYr].y;Don't need this
+						d.offset = Math.round(x(d[maxYr].x) / x.range()[1] * 100);
+						return Math.min(95, Math.max(5, d.offset))+'%';
+						d.offset = x(d[maxYr].x);
+						
+						var id = textpathHref(d, i);
+						var thePath = g.select('defs').select(id).node();
+						console.log("thePath");
+						console.log(id);
+						console.log(thePath);
+						console.log("x="+x(d[maxYr].x));
+						console.log("offset="+d.offset);
+						console.log(thePath.getPointAtLength(d.offset).x);
+						while (thePath.getPointAtLength(d.offset).x < x(d[maxYr].x)) {
+							d.offset += 1;
+						}
+						return d.offset;
+					})
+					.attr('text-anchor', function(d) {
+						//return d.offset <= x.range()[0] ? 'start' : d.offset >= x.range()[1] ? 'end' : 'middle';
+						//return 'middle';
+						return d.offset > 90 ? 'end' : d.offset < 10 ? 'start' : 'middle';
+					})
+					.text(function(d){ return d[0].group; })
+					.style("font-size","12px")
+					.style("font-family","Arial, Helvetica")
+					.style("font-weight","normal")
+			;
+		}
+
+		function sortBy(a,b){
+			if (sort() == 'value (descending)') return a.y - b.y;
+			if (sort() == 'value (ascending)') return b.y - a.y;
+			if (sort() == 'group') return a.group - b.group;
+		}
+
+		function interpolate(points) {
+			var x0 = points[0][0], y0 = points[0][1], x1, y1, x2,
+				path = [x0, ",", y0],
+				i = 0,
+				n = points.length;
+
+			while (++i < n) {
+				x1 = points[i][0], y1 = points[i][1], x2 = (x0 + x1) / 2;
+				path.push("C", x2, ",", y0, " ", x2, ",", y1, " ", x1, ",", y1);
+				x0 = x1, y0 = y1;
+			}
+			return path.join("");
+		}
+	});
 
 })();
